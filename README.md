9a194c3204a4dd236d533b94e9f4efc8777a5941
Linda Kovacs <lindaprovenza@yahoo.com>
2019-04-01 17:50:23 -0400

    Retro App almost done

--- README.md
+++ README.md
@@ -1,3 +1,70 @@
+# Retro-Board
+**[AlbanyCanCode Course](https://albanycancode.org/)**
+- Projects: [1 Retrospective Board](https://docs.google.com/document/d/1ePLNNU1BKLD9WByelb7WyfdGpntwhpDIKn0KkhGxboI/edit?usp=sharing)
+- Instructors: [Matina Patsos](https://github.com/matinaspatsos) and [Jamal Taylor](https://github.com/Louis345)
+- [AlbanyCanCode's JavaScript Frameworks Class Spring 2019](https://github.com/AlbanyCanCodeCourses/JavaScript2019)
+- Inspired by the [FunRetro retrospective board](https://funretro.io)
+- [Demo Retro Board on codesandbox.io](https://codesandbox.io/s/q706r0o5p6)
+
+## Introduction
+
+**Retro-Board** is a *ReactJS* application that sets up a retrospective dashboard tool that can de used for sprint sessions keep track and discuss what went well and what didn’t. The tool allows teams to easily identify obstacles and discuss ideas for improvements in sprint projects.
+
+There are three caterories:
+
+    - Worked Well: identify items that worked well
+    - To Improve: identify items that need to be improved
+    - Action Items: identify items that need to addressed and follwed up on in future sprints
+
+Each category contains various cards of items that can be inserted, deleted and moved from category to another category.
+
+![Retro Board](/src/boardExample.png)
+
+## Installation
+
+Clone or Download the `github` repo into your local computer directory. Make sure that `yarn` is installed.
+
+### Install Packages
+
+Change directory into the `retro-board` directory and install packages:
+
+    $>cd retro-board
+    $>yarn install
+
+### Starting Application
+
+After the installation of the packages, start the ReactJS Server to run on your computer at `http://localhost:3000`
+
+    $>yarn start
+
+## Getting Started
+
+Each category allows you to add a new card, delete existing cards, and move cards to the right or left category colum.
+
+### Add New Card
+
+Click on `+` above the category to add a new card and enter text for the item. Click on `ADD` button to save the item content. The new card textbox is toggled on via the `+` button or you can remove it with the `delete` button.
+
+The textbox validates that it is not empty with an `alert`.
+
+### Delete Card
+
+Click on `x` at the bottom of each card to delete the card.
+
+### Move Card
+
+Click on `<` to move the current card to the category to the left or `>` to move the current card to the category to the right. The cards will be moved around when at the front or at the end of the columns.
+
+For example, if the card is in the last column or `Action Items` category, moving to the right will push the card to the first column or `Went Well` category. Also, if the card is in the first column or `Went Well` category, moving to the left will push the card to the last column or `Action Items` category.
+
+### Toggle between horizontal and vertical board display
+Description - coming soon
+
+### Likes and Dislikes
+Description - coming soon
+
+# Retro Board App
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -67,3 +134,10 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
 
+## Author
+
+**[Linda Kovacs](https://github.com/lindakovacs)**
+
+## Acknowledgments
+
+* A big Thank You to [Matina Patsos](https://github.com/matinaspatsos) and [Jamal Taylor](https://github.com/Louis345) for their expert instruction guidance in AlbanyCanCode Bootcamp!
--- src/App.js
+++ src/App.js
@@ -1,25 +1,268 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Card from "./components/Card/Card";
 
 class App extends Component {
+  state = {
+    categories: {
+      wentWell: [],
+      toImprove: [],
+      actionItems: []
+    },
+    addClass: false,
+    userInput: "",
+    id: 0,
+    Cards: [],
+    likes: 0,
+    dislikes: 0
+  };
+
+  userInput = (e, idx) => {
+    let newCards = [...this.state.Cards];
+    newCards[idx].input = e.target.value;
+    this.setState({
+      Cards: newCards
+    });
+  };
+
+  validateInput = e => {
+    if (e.target.value === "") {
+      window.alert("input required");
+    }
+  };
+  Delete = id => {
+    this.setState({
+      Cards: this.state.Cards.filter(card => card.id !== id)
+    });
+  };
+
+  CreateCard = (type, input) =>
+    this.setState({
+      Cards: [
+        ...this.state.Cards,
+        {
+          id: this.state.id,
+          type: type,
+          input: input,
+          likes: 0,
+          dislikes: 0
+        }
+      ],
+      id: this.state.id + 1
+    });
+
+  MoveLeft = (id, idx) => {
+    let newCards = [...this.state.Cards];
+    for (let card of newCards) {
+      if (card.id === id && card.type === "Went Well") {
+        card.type = "Action Items";
+      } else if (card.id === id && card.type === "To Improve") {
+        card.type = "Went Well";
+      } else if (card.id === id && card.type === "Action Items") {
+        card.type = "To Improve";
+      }
+    }
+    newCards.push(newCards[idx]);
+    newCards.splice(idx, 1);
+    this.setState({
+      Cards: newCards
+    });
+  };
+
+  MoveRight = (id, idx) => {
+    let newCards = [...this.state.Cards];
+    for (let card of newCards) {
+      if (card.id === id && card.type === "Went Well") {
+        card.type = "To Improve";
+      } else if (card.id === id && card.type === "To Improve") {
+        card.type = "Action Items";
+      } else if (card.id === id && card.type === "Action Items") {
+        card.type = "Went Well";
+      }
+    }
+    newCards.push(newCards[idx]);
+    newCards.splice(idx, 1);
+    this.setState({
+      Cards: newCards
+    });
+  };
+
+  handleLikes = idx => {
+    let newCards = [...this.state.Cards];
+    newCards[idx].likes++;
+    this.setState({
+      Cards: newCards
+    });
+  };
+
+  handleDislikes = idx => {
+    let newCards = [...this.state.Cards];
+    newCards[idx].dislikes++;
+    this.setState({
+      Cards: newCards
+    });
+  };
+
+  toggle = () => {
+    if (this.state.addClass === 2) {
+      this.setState({ addClass: 0 });
+    } else {
+      this.setState({ addClass: this.state.addClass + 1 });
+    }
+  };
+
+  // toggle() {
+  //   this.setState({ addClass: !this.state.addClass });
+  // }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <h2>Retro Board</h2> <br />
+        <div className="btn-cntr">
+          <button onClick={this.toggle.bind(this)} className="ChangeLayout">
+            <i className="fas fa-bars" />
+          </button>
+        </div>
+        <br />
+        <div className="text-center">
+          <div
+            className={
+              this.state.addClass === 1
+                ? "col"
+                : this.state.addClass === 2
+                ? "col"
+                : "row"
+            }
           >
-            Learn React
-          </a>
-        </header>
+            <div className={this.state.addClass === 1 ? "row" : "col"}>
+              <div
+                className={(this.state.addClass === 1
+                  ? " Rotate-1"
+                  : ""
+                ).toString()}
+              >
+                <h4>Went Well</h4>
+                <button
+                  type="button"
+                  className="addButton"
+                  onClick={() => this.CreateCard("Went Well", "")}
+                >
+                  +
+                </button>
+              </div>
+              {this.state.Cards.map((card, idx) => {
+                if (card.type === "Went Well") {
+                  return (
+                    <Card
+                      key={"Went Well" + idx}
+                      idx={idx}
+                      cardId={card.id}
+                      value={card.input}
+                      userInput={this.userInput}
+                      validateInput={this.validateInput}
+                      MoveLeft={this.MoveLeft}
+                      Delete={this.Delete}
+                      MoveRight={this.MoveRight}
+                      likesCount={card.likes}
+                      dislikesCount={card.dislikes}
+                      handleLikes={this.handleLikes}
+                      handleDislikes={this.handleDislikes}
+                      addClass={this.state.addClass}
+                      color={"wentWell"}
+                    />
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </div>
+            <div className={this.state.addClass === 1 ? "row" : "col"}>
+              <div
+                className={(this.state.addClass === 1
+                  ? " Rotate-1"
+                  : ""
+                ).toString()}
+              >
+                <h4>To Improve</h4>
+                <button
+                  type="button"
+                  className="addButton"
+                  onClick={() => this.CreateCard("To Improve", "")}
+                >
+                  +
+                </button>
+              </div>
+              {this.state.Cards.map((card, idx) => {
+                if (card.type === "To Improve") {
+                  return (
+                    <Card
+                      key={"To Improve" + idx}
+                      idx={idx}
+                      cardId={card.id}
+                      value={card.input}
+                      userInput={this.userInput}
+                      validateInput={this.validateInput}
+                      MoveLeft={this.MoveLeft}
+                      Delete={this.Delete}
+                      MoveRight={this.MoveRight}
+                      likesCount={card.likes}
+                      dislikesCount={card.dislikes}
+                      handleLikes={this.handleLikes}
+                      handleDislikes={this.handleDislikes}
+                      addClass={this.state.addClass}
+                      color={"toImprove"}
+                    />
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </div>
+            <div className={this.state.addClass === 1 ? "row" : "col"}>
+              <div
+                className={(this.state.addClass === 1
+                  ? " Rotate-1"
+                  : ""
+                ).toString()}
+              >
+                <h4>Action Items</h4>
+                <button
+                  type="button"
+                  className="addButton"
+                  onClick={() => this.CreateCard("Action Items", "")}
+                >
+                  +
+                </button>
+              </div>
+              {this.state.Cards.map((card, idx) => {
+                if (card.type === "Action Items") {
+                  return (
+                    <Card
+                      key={"Action Items" + idx}
+                      idx={idx}
+                      cardId={card.id}
+                      value={card.input}
+                      userInput={this.userInput}
+                      validateInput={this.validateInput}
+                      MoveLeft={this.MoveLeft}
+                      Delete={this.Delete}
+                      MoveRight={this.MoveRight}
+                      likesCount={card.likes}
+                      dislikesCount={card.dislikes}
+                      handleLikes={this.handleLikes}
+                      handleDislikes={this.handleDislikes}
+                      addClass={this.state.addClass}
+                      color={"actionItems"}
+                    />
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
